c8c4318c2311f6b1e6a3d4ee006a803cea824727
Aravindsys <kuaravind1999k@gmail.com>
2021-02-08 19:27:40 +0530

    lopr

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-app.use(express.static('./'));
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: './'}
-  );
-  });
-
-app.listen(process.env.PORT || 8080);
